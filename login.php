3b7817eab994a4603c7543b170bed4881dec9151
harishashim15 <harisomniverse2@gmail.com>
2026-05-05 14:28:32 +0800

    host database and connect

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+$host = "sql207.infinityfree.com";
+$user = "if0_41833955";
+$pass = "PassFYP15";
+$db   = "if0_41833955_XXX";
+$port = 3306;
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT role FROM users WHERE username=? AND password=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($row = $result->fetch_assoc()) {
+    echo $row['role'];
+} else {
+    echo "invalid";
+}
+
+$conn->close();
+?>
