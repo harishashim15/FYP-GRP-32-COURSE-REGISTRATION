e26490925775f915b7d0f6b1bd2a641afd2ad678
harishashim15 <harisomniverse2@gmail.com>
2026-05-30 10:37:43 +0800

    Adjustment of Admin SIdebar

--- admin/edit_subject.php
+++ admin/edit_subject.php
@@ -105,7 +105,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         .system-title { color: #ffc107; font-size: 16px; font-weight: 600; margin-top: 12px; }
         .menu a {
             display: flex; align-items: center; gap: 15px;
-            text-decoration: none; color: white; padding: 12px 20px;
+            text-decoration: none; color: white; padding: 9px 20px;
             border-radius: 14px; margin-bottom: 12px; transition: 0.3s; font-size: 16px;
         }
         .menu a:hover, .menu .active { background: linear-gradient(to right, #f4a000, #e08700); }
