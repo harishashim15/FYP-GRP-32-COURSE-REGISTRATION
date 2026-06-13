3cc66c7e3ded7e33147d2ca79c183b70bed54582
AnaqiNazri <anaqinajmi07@gmail.com>
2026-06-14 02:36:55 +0800

    change

--- api/session.php
+++ api/session.php
@@ -3,7 +3,14 @@ require_once __DIR__ . '/config/database.php';
 
 $user = getCurrentUser();
 if ($user) {
-    echo json_encode(['id' => $user['id'], 'name' => $user['name'], 'email' => $user['email'], 'role' => $user['role'], 'matrix_number' => $user['matrix_number'] ?? null]);
+    // FIXED: Use 'user_name' instead of 'name'
+    echo json_encode([
+        'id' => $user['id'], 
+        'name' => $user['user_name'],  // CHANGED: 'name' to 'user_name'
+        'email' => $user['email'], 
+        'role' => $user['role'], 
+        'matrix_number' => $user['matrix_number'] ?? null
+    ]);
 } else {
     http_response_code(401);
     echo json_encode(['error' => 'Not logged in']);
